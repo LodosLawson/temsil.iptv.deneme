e6171fb469b6306f40e7d3902850f6792db35b54
Lodos Lawson <68427935+LodosLawson@users.noreply.github.com>
2023-10-07 00:07:02 +0200

    Update script.js
    Signed-off-by: Lodos Lawson  <68427935+LodosLawson@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,47 +1,30 @@
-// script.js dosyası
-const iptvUrlInput = document.getElementById("iptvUrl");
-const getKanalListesiBtn = document.getElementById("getKanalListesiBtn");
-const kanalListesi = document.getElementById("kanalListesi");
-const videoOynatici = document.getElementById("videoOynatici");
+// IPTV URL'sini burada belirtin
+const iptvUrl = "http://pray45748.cdngold.me/get.php?username=7c8c640943&password=4574cdce6e&type=m3u_plus&output=ts";
 
-const httpURL = "http://pray45748.cdngold.me/get.php?username=7c8c640943&password=4574cdce6e&type=m3u_plus&output=ts";
-
-fetch(httpURL)
+// Kanal listesini almak için bir HTTP isteği yapın
+fetch(iptvUrl)
     .then(response => response.text())
     .then(data => {
-        console.log(data); // Veriyi konsola yazdırın
+        // M3U8 dosyasını satırlara bölelim
+        const lines = data.split("\n");
+        const kanalListesi = [];
+
+        for (let i = 0; i < lines.length; i++) {
+          alert(lines[i]);
+            if (lines[i].startsWith("#EXTINF")) {
+                // Kanal başlığı ise, bir sonraki satır URL'dir
+                kanalListesi.push(lines[i + 1]);
+            }
+        }
+
+        // Kanal listesini HTML'de görüntüle
+        const kanalListesiElementi = document.getElementById("kanalListesi");
+        kanalListesi.forEach((kanal, index) => {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `<a href="${kanal}" target="_blank">Kanal ${index + 1}</a>`;
+            kanalListesiElementi.appendChild(listItem);
+        });
     })
-    .catch(error => console.error(error));
-
-getKanalListesiBtn.addEventListener("click", () => {
-    const iptvUrl = iptvUrlInput.value;
-
-    // IPTV kanallarını çekme
-    fetch(iptvUrl)
-        .then(response => response.text())
-        .then(data => {
-            const lines = data.split('\n');
-            let kanalAdi = '';
-            let kanalURL = '';
-
-            // Mevcut kanal listesini temizle
-            kanalListesi.innerHTML = '';
-
-            lines.forEach(line => {
-                if (line.startsWith('#EXTINF')) {
-                    kanalAdi = line.split(',')[1];
-                } else if (line.trim() !== '') {
-                    kanalURL = line;
-                    const kanalItem = document.createElement("li");
-                    kanalItem.textContent = kanalAdi;
-                    kanalItem.addEventListener("click", () => {
-                        videoOynatici.src = kanalURL;
-                        videoOynatici.load();
-                        videoOynatici.play();
-                    });
-                    kanalListesi.appendChild(kanalItem);
-                }
-            });
-        })
-        .catch(error => console.error(error));
-});
+    .catch(error => {
+        console.error("Hata:", error);
+    });
